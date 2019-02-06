39b8eb272e1495fee37c2d49c375d8050891002e
Nayema Nasir <nayema.nasir89@gmail.com>
2019-02-05 23:00:35 -0500

    Add the ability to provide a number as an argument

--- weather-sms.js
+++ weather-sms.js
@@ -1,16 +1,25 @@
-const request = require('request')
+const client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN)
 
-const accuWeatherApiKey = process.env.ACCUWEATHER_API
+const request = require('request')
 const accuWeatherRequestURL =
   'http://dataservice.accuweather.com/currentconditions/v1/55488?apikey='
-  + accuWeatherApiKey
+  + process.env.ACCUWEATHER_API
   + '&details=true'
+
+console.log(process.argv)
 
 request(accuWeatherRequestURL, (error, response, body) => {
   if (!error && response.statusCode === 200) {
     const currentConditions = JSON.parse(body)
     const realFeel = currentConditions[0]['RealFeelTemperature']['Metric']['Value']
 
-    console.log(realFeel)
+    client.messages
+      .create({
+        body: `It is ${realFeel} outside!`,
+        from: process.env.TWILIO_NUMBER,
+        to: process.argv[2]
+      })
+      .then(message => console.log(message.sid))
+      .done();
   }
 })
