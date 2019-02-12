f6b49c71ad4d02c5e8c1f6d571022b0df061493d
Nayema Nasir <nayema.nasir89@gmail.com>
2019-02-11 21:59:23 -0500

    Conver code to using callback chaining

--- weather-sms.js
+++ weather-sms.js
@@ -6,95 +6,55 @@ const currentConditionsRequestURL =
   + process.env.ACCUWEATHER_API
   + '&details=true'
 
-const next12HourForecastRequestURL =
-  'http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/55488?apikey='
-  + process.env.ACCUWEATHER_API
-  + '&details=true'
-  + '&metric=true'
-
 const dailyForecastRequestURL =
   'http://dataservice.accuweather.com/forecasts/v1/daily/1day/55488?apikey='
   + process.env.ACCUWEATHER_API
   + '&details=true'
   + '&metric=true'
 
+let message = ''
 request(currentConditionsRequestURL, (error, response, body) => {
   if (!error && response.statusCode === 200) {
     const conditions = JSON.parse(body)
     const realFeel = conditions[0]['RealFeelTemperature']['Metric']['Value']
     const weatherText = conditions[0]['WeatherText']
 
-    const message = `It is ${realFeel}°C and ${weatherText} outside!`
-    console.log(message)
+    message += `It is ${realFeel}°C and ${weatherText} outside!`
 
-    client.messages
-      .create({
-        body: message,
-        from: process.env.TWILIO_NUMBER,
-        to: process.argv[2]
-      })
-      .then(message => console.log(message.sid))
-      .done()
-  }
-})
+    request(dailyForecastRequestURL, (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        const conditions = JSON.parse(body)
 
-request(dailyForecastRequestURL, (error, response, body) => {
-  if (!error && response.statusCode === 200) {
-    const conditions = JSON.parse(body)
+        const minTemp = conditions['DailyForecasts'][0]['RealFeelTemperature']['Minimum']['Value']
+        const maxTemp = conditions['DailyForecasts'][0]['RealFeelTemperature']['Maximum']['Value']
 
-    const minTemp = conditions['DailyForecasts'][0]['RealFeelTemperature']['Minimum']['Value']
-    const maxTemp = conditions['DailyForecasts'][0]['RealFeelTemperature']['Maximum']['Value']
+        const dayPhrase = conditions['DailyForecasts'][0]['Day']['LongPhrase']
+        const daySnowProbability = conditions['DailyForecasts'][0]['Day']['SnowProbability']
+        const dayRainProbability = conditions['DailyForecasts'][0]['Day']['RainProbability']
 
-    const dayPhrase = conditions['DailyForecasts'][0]['Day']['LongPhrase']
-    const daySnowProbability = conditions['DailyForecasts'][0]['Day']['SnowProbability']
-    const dayRainProbability = conditions['DailyForecasts'][0]['Day']['RainProbability']
+        const nightPhrase = conditions['DailyForecasts'][0]['Night']['LongPhrase']
+        const nightSnowProbability = conditions['DailyForecasts'][0]['Night']['SnowProbability']
+        const nightRainProbability = conditions['DailyForecasts'][0]['Night']['RainProbability']
 
-    const nightPhrase = conditions['DailyForecasts'][0]['Night']['LongPhrase']
-    const nightSnowProbability = conditions['DailyForecasts'][0]['Night']['SnowProbability']
-    const nightRainProbability = conditions['DailyForecasts'][0]['Night']['RainProbability']
+        message += `
+            \nToday's forecast:
+            \nhighest: ${maxTemp}°C
+            \nlowest: ${minTemp}°C
+            \n${dayPhrase} with snow probability of ${daySnowProbability}% and rain probability of ${dayRainProbability}%.
+            \n${nightPhrase} with snow probability of ${nightSnowProbability}% and rain probability of ${nightRainProbability}%.
+             `
 
-    const message = `
-    \nToday's forecast:
-    \nhighest: ${maxTemp}°C
-    \nlowest: ${minTemp}°C
-    \n${dayPhrase} with snow probability of ${daySnowProbability}% and rain probability of ${dayRainProbability}%.
-    \n${nightPhrase} with snow probability of ${nightSnowProbability}% and rain probability of ${nightRainProbability}%.
-    `
+        console.log(message)
 
-    console.log(message)
-
-    client.messages
-      .create({
-        body: message,
-        from: process.env.TWILIO_NUMBER,
-        to: process.argv[2]
-      })
-      .then(message => console.log(message.sid))
-      .done()
-  }
-})
-
-request(next12HourForecastRequestURL, (error, response, body) => {
-  if (!error && response.statusCode === 200) {
-    const conditions = JSON.parse(body)
-    const realFeelin1Hour = conditions[0]['RealFeelTemperature']['Value']
-    const realFeelin9Hours = conditions[7]['RealFeelTemperature']['Value']
-    const iconPhrasein1Hour = conditions[0]['IconPhrase']
-    const iconPhrasein9Hours = conditions[7]['IconPhrase']
-
-    const message = `
-    \nOn your way to work it will be ${realFeelin1Hour}°C and ${iconPhrasein1Hour}.
-    \nOn your way home it will be ${realFeelin9Hours}°C and ${iconPhrasein9Hours}.
-    `
-    console.log(message)
-
-    client.messages
-      .create({
-        body: message,
-        from: process.env.TWILIO_NUMBER,
-        to: process.argv[2]
-      })
-      .then(message => console.log(message.sid))
-      .done()
+        client.messages
+          .create({
+            body: message,
+            from: process.env.TWILIO_NUMBER,
+            to: process.argv[2]
+          })
+          .then(message => console.log(message.sid))
+          .done()
+      }
+    })
   }
 })
